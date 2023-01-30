a9ff9f09209266a2d0adf98fe9ef4ce4ef9ab269
Mizanur Rahman <nickty.86@gmail.com>
2023-01-30 12:03:44 +0600

    Areas of Consulting

--- front-page.php
+++ front-page.php
@@ -5,24 +5,173 @@
 <div class="container">
   <div class="hero">
     <div class="hero-text">
-        <div class="body-text">
-           <h1>
-           Hello,
-            I’m Shumaila Ali
-           </h1>
-           <p>
-           Strategic Business Partner | People, Culture & Engagement | Diversity & Inclusion Advocate CAHRI
-           </p>
-        </div>
+      <div class="body-text">
+        <h1>
+          Hello,
+          I’m Shumaila Ali
+        </h1>
+        <p>
+          Strategic Business Partner | People, Culture & Engagement | Diversity & Inclusion Advocate CAHRI
+        </p>
+      </div>
     </div>
     <div class="hero-image">
-        <div class="shumaila-image">
-        <img src="<?php echo get_stylesheet_directory_uri();?>/images/shumaila.png">
-        </div>
+      <div class="shumaila-image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/shumaila.png">
+      </div>
     </div>
-   
+
   </div>
 </div>
+
+<div class="container">
+  <div class="about">
+    <h3 class="text-center">About Me</h3>
+    <div>
+      <p>Shumaila is an outcomes focused Strategic Business Partner with expertise in stakeholder management, leadership
+        development, organisational change management, workforce transition, employee engagement, talent acquisition and
+        retention, HR technology, organisational communications, capability development, project management and policy
+        development & review.</p>
+
+      <p>She strives for challenge and growth, both personally and professionally and have extensive experience
+        partnering, coaching and advising Business owners, Directors, leadership teams and senior executives to achieve
+        commercially oriented business and people outcomes in fast paced environments. With her experience, Shumaila is
+        able to effortlessly communicate and engage with a diverse range of stakeholders on a variety of projects,
+        whilst leading business and people practices that provide an employee-oriented and high performance culture.</p>
+
+      <p>Shumaila has been recognised by numerous industry and professional associations for her work. She was nominated
+        as a finalist for the Telstra Business Women's Award for Government and Academia, in addition to being a
+        finalist for Australian Institute of Management (AIM)'s prestigious 30under30 list and the Young Manager award;
+        and for the Sydney City Business Awards as a "Young Business Executive". More recently her work at NBRS was
+        recognised by the Australian Institute of Architects as a winner for the bi-annual Best in Practice Award for
+        2022 and as a finalist for the same award in 2020.</p>
+    </div>
+  </div>
+</div>
+
+<!-- Areas of consulting section -->
+<section class="consulting">
+  <div class="container">
+    <div class="consulting-context">
+      <h2>Areas of Consulting</h2>
+      <p>Nibh lacus quam non non neque proin vel in odio. Ac elementum odio mattis imperdiet tincidunt tellus purus.</p>
+
+      <div class="row">
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/employee-communication.png">
+          <h3>
+            Employee Communication
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/employee-complaints-and-grievances.png">
+          <h3>
+            Employee Complaints and Grievances
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/employee-onboarding.png">
+          <h3>
+            Employee Onboarding and Induction
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/hr-administration.png">
+          <h3>
+            HR Administration & Compliance
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/hr-policies.png">
+          <h3>
+            HR Policies and Processes
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/hr-system.png">
+          <h3>
+            HRIS and HR systems
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/job-profiling.png">
+          <h3>
+            Job profiling and description
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/kpi.png">
+          <h3>
+            KPIs and Goal Setting
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/performance-management.png">
+          <h3>
+            Performance Management
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/workforce.png">
+          <h3>
+            Workforce Development
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/diversity.png">
+          <h3>
+            Diversity & Inclusion
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+        <div class="col-sm">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/employee-wellbeing.png">
+          <h3>
+            Employee Wellbeing
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-sm-4">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icons/employee-engagement.png">
+          <h3>
+            Employee Engagement and Workplace Culture
+          </h3>
+          <p>Et eget nibh elit volutpat. Mauris elementum massa orci sollicitudin lobortis fusce.</p>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</section>
 
 
 
